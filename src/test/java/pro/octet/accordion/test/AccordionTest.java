647651cf3ec04975036e5fe3a566cdc14df04f28
william <william@octet.pro>
2023-12-15 15:57:07 +0800

    Optimized accordion

--- src/test/java/pro/octet/accordion/test/AccordionTest.java
+++ src/test/java/pro/octet/accordion/test/AccordionTest.java
@@ -4,31 +4,34 @@ import pro.octet.accordion.Accordion;
 import pro.octet.accordion.AccordionPlan;
 import pro.octet.accordion.action.model.ActionConfig;
 import pro.octet.accordion.core.enums.ActionType;
+import pro.octet.accordion.utils.CommonUtils;
 
 public class AccordionTest {
 
     public static void main(String[] args) {
-        ActionConfig a = ActionConfig.builder().id("a").actionType(ActionType.TEST).actionName("A").build();
-        ActionConfig b = ActionConfig.builder().id("b").actionType(ActionType.TEST).actionName("B").build();
-        ActionConfig c = ActionConfig.builder().id("c").actionType(ActionType.TEST).actionName("C").build();
-        ActionConfig d = ActionConfig.builder().id("d").actionType(ActionType.TEST).actionName("D").build();
-        ActionConfig e = ActionConfig.builder().id("e").actionType(ActionType.TEST).actionName("E").build();
-        ActionConfig f = ActionConfig.builder().id("f").actionType(ActionType.TEST).actionName("F").build();
-        ActionConfig g = ActionConfig.builder().id("g").actionType(ActionType.TEST).actionName("G").build();
+        ActionConfig a = ActionConfig.builder().id(CommonUtils.randomString("ACT").toUpperCase()).actionType(ActionType.TEST).actionName("A").build();
+        ActionConfig b = ActionConfig.builder().id(CommonUtils.randomString("ACT").toUpperCase()).actionType(ActionType.TEST).actionName("B").build();
+        ActionConfig c = ActionConfig.builder().id(CommonUtils.randomString("ACT").toUpperCase()).actionType(ActionType.TEST).actionName("C").build();
+        ActionConfig d = ActionConfig.builder().id(CommonUtils.randomString("ACT").toUpperCase()).actionType(ActionType.TEST).actionName("D").build();
+        ActionConfig e = ActionConfig.builder().id(CommonUtils.randomString("ACT").toUpperCase()).actionType(ActionType.TEST).actionName("E").build();
+        ActionConfig f = ActionConfig.builder().id(CommonUtils.randomString("ACT").toUpperCase()).actionType(ActionType.TEST).actionName("F").build();
+        ActionConfig g = ActionConfig.builder().id(CommonUtils.randomString("ACT").toUpperCase()).actionType(ActionType.TEST).actionName("G").build();
+        ActionConfig h = ActionConfig.builder().id(CommonUtils.randomString("ACT").toUpperCase()).actionType(ActionType.TEST).actionName("H").build();
 
         AccordionPlan plan = AccordionPlan.of()
                 .start(a)
                 .next(a, b, c)
                 .next(b, d)
                 .next(c, d)
-                .next(d, e, f)
-                .next(e, g)
-                .next(f, g);
+                .next(d, e, f, g)
+                .next(e, h)
+                .next(f, h)
+                .next(g, h);
 
-        System.out.println(plan.getPlanGraphView());
+        System.out.println("Execute:");
         Accordion accordion = new Accordion();
         accordion.play(plan);
-        System.out.println(accordion.verbose());
+        System.out.println("Accordion plan:\n" + accordion.verbose());
 
     }
 }
